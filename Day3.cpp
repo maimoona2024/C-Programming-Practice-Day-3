8fd39df4efc5a5ea59f326ae86fd62df6d1f09ee
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2024-04-09 18:12:43 +0500

    Add files via upload

---
+++ Day3.cpp
@@ -0,0 +1,43 @@
+//Program # 1 of Day 3 Post-Increment plus operator.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int x = 5;
+//     cout<<x<<endl;  //5
+//     cout<<x++<<endl; //5
+//     cout<<x<<endl; //4
+// }
+
+
+//Program # 2 of Day 3 Pre-Increment plus operator.
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int x = 5;
+//     cout<<x<<endl; //5
+//     cout<<++x<<endl; //6
+//     cout<<x<<endl; //6
+// }
+
+//Program # 3 of Day 3 Pre-Decrement minus operator
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int x = 5;
+//     cout<<x<<endl; //5
+//     cout<<--x<<endl; //4
+//     cout<<x<<endl; //4
+// }
+
+//Program # 4 of Day 3 Post-Decrement minus operator
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int x = 5;
+//     cout<<x<<endl; //5
+//     cout<<x--<<endl; //5
+//     cout<<x<<endl; //4
+// }
